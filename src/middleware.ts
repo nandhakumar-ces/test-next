0c3c835fcc8143a7f1d3383e11c471ceea4003b5
nandhakumar-ces <91317519+nandhakumar-ces@users.noreply.github.com>
2025-02-13 18:49:19 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req: Request) {
+export function middleware() {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
   const response = NextResponse.next();
