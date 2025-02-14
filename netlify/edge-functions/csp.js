ed73530b114034eb70b0d067496ee66104331c70
nandhakumar-ces <91317519+nandhakumar-ces@users.noreply.github.com>
2025-02-14 14:19:12 +0530

    Update csp.js

--- netlify/edge-functions/csp.js
+++ netlify/edge-functions/csp.js
@@ -1,16 +1,12 @@
 export default async (request, context) => {
     const response = await context.next();
-    
-    // Get existing CSP header
+
     const existingCsp = response.headers.get("Content-Security-Policy") || "";
-    
-    // Your custom CSP
+
     const customCsp = "img-src 'self' blob: data:; upgrade-insecure-requests; script-src 'sha256-test'";
 
-    // Merge CSP
     const mergedCsp = existingCsp ? `${existingCsp}; ${customCsp}` : customCsp;
 
-    // Set the merged CSP in response headers
     response.headers.set("Content-Security-Policy", mergedCsp);
     
     return response;
