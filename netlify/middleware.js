60a17b4a5313cd8f54752ef51b19b247f7470e91
nandhakumar-ces <91317519+nandhakumar-ces@users.noreply.github.com>
2025-02-20 20:24:26 +0530

    Delete netlify/middleware.js

--- netlify/middleware.js
+++
@@ -1,7 +0,0 @@
-import { adapter } from '@next/server/api-adapter';
-
-import { middleware as nextMiddleware } from '../src/middleware';
-
-export const handler = adapter.apiHandler(async (req, res) => {
-  await nextMiddleware(req, res);
-});
